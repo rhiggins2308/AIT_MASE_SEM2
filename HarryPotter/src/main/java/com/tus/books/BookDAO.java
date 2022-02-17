b9548be4ef28143525178bb7296f9eb5d19778a4
rhiggins2308 <rhiggins2308@gmail.com>
2022-02-17 14:56:59 +0000

    WebDev CA Assessment Setup

--- HarryPotter/src/main/java/com/tus/books/BookDAO.java
+++ HarryPotter/src/main/java/com/tus/books/BookDAO.java
@@ -1,5 +1,26 @@
 package com.tus.books;
 
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+@LocalBean
 public class BookDAO {
 
+	@PersistenceContext
+	private EntityManager em;
+	public List<Book> getAllBooks() {
+		Query query = em.createQuery("SELECT b FROM Book b");
+		return query.getResultList();
+		
+	}
+
+	public Book getBook(int id) {
+		return em.find(Book.class, id);
+	}
 }
--- HarryPotter/src/main/java/com/tus/books/BookWS.java
+++ HarryPotter/src/main/java/com/tus/books/BookWS.java
@@ -1,5 +1,37 @@
 package com.tus.books;
 
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/books")
+@Stateless
+@LocalBean
 public class BookWS {
 
+	@EJB
+	private BookDAO bookDao;
+	
+	@GET
+	@Produces ({MediaType.APPLICATION_JSON})
+	public Response findAll() {
+		List<Book> books = bookDao.getAllBooks();
+		return Response.status(200).entity(books).build();
+	}
+
+	@GET
+	@Produces ({MediaType.APPLICATION_JSON})
+	@Path("/{id}")
+	public Response fineCarById(@PathParam("id") int id) {
+		Book book = bookDao.getBook(id);
+		return Response.status(200).entity(book).build();
+	}
 }
